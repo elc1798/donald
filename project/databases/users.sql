18d3670e7d914ba35fa031075d08cce50107004a
David Song <songdavid98@gmail.com>
2015-10-09 20:05:17 -0400

    added users

---
+++ project/databases/users.sql
@@ -0,0 +1,9 @@
+create table users(username text, password text);
+
+insert into users('Obama', 'michelle');
+insert into users('Ben Carson', 'wannabeprez');
+insert into users('Minnie', 'love');
+insert into users('Mickey', 'mouse');
+insert into users('G0fy', '1234');
+insert into users('TRUMPSUPERFAN', 'TRUMP');
+insert into users('A_Potato', 'DS');
