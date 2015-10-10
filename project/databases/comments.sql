37ed74de2649cbdd0b098205a496614b5e0cbac3
David Song <songdavid98@gmail.com>
2015-10-09 20:07:30 -0400

    capitalized letters

--- project/databases/comments.sql
+++ project/databases/comments.sql
@@ -1,6 +1,6 @@
 create table comments(posttitle text, body text, username text,);
 
-insert into comments('We vote for Mr. Trump!', 'obviously fake', 'ben carson');
-insert into comments('We vote for Mr. Trump!', 'that one person', 'obama');
-insert into comments('donald duck is retarded', 'that is bullying', 'minnie');
-insert into comments('Donald 2stronk', 'my best friend! #kindomhearts', 'g0fy');
+insert into comments('We vote for Mr. Trump!', 'obviously fake', 'Ben Carson');
+insert into comments('We vote for Mr. Trump!', 'that one person', 'Obama');
+insert into comments('donald duck is retarded', 'that is bullying', 'Minnie');
+insert into comments('Donald 2stronk', 'my best friend! #kindomhearts', 'G0fy');
