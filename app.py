e1ed44ccd58c6627caca3d1569fc2aa1933ed138
Chris Grant <chris@chrisgrant.co>
2015-10-12 17:50:47 -0400

    Added routes and templates to conform to spec

--- app.py
+++ app.py
@@ -6,6 +6,30 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-if __name__=="__main__":
+@app.route('/@<username>')
+def profile():
+	return render_template('profile.html')
+
+@app.route('/@<username>/<post>')
+def read():
+	return render_template('post.html')
+
+@app.route('/@<username>/<post>/comments', methods=['POST'])
+def comment():
+	return 'Coming Soon';
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    return render_template('new.html')
+
+if __name__=='__main__':
     app.debug = True
     app.run(host='0.0.0.0', post = 8000)
