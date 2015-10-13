d18156e4f2e39564ca6a693bf47d02ecaa327ba2
David Song <songdavid98@gmail.com>
2015-10-13 11:47:06 -0400

    updated dates

--- databases/comments.sql
+++ databases/comments.sql
@@ -1,6 +1,6 @@
 create table comments(posttitle text, body text, username text,);
 
-insert into comments('We-vote-for-Mr.-Trump!', 'obviously fake', 'Ben Carson', date);
-insert into comments('We-vote-for-Mr.-Trump!', 'that one person', 'Obama', date);
-insert into comments('donald-duck-is-retarded', 'that is bullying', 'Minnie', date);
-insert into comments('Donald-2stronk', 'my best friend! #kindomhearts', 'G0fy', date);
+insert into comments('We-vote-for-Mr.-Trump!', 'obviously fake', 'Ben Carson', '2015-10-12');
+insert into comments('We-vote-for-Mr.-Trump!', 'that one person', 'Obama', '2015-10-12');
+insert into comments('donald-duck-is-retarded', 'that is bullying', 'Minnie', '2014-12-25');
+insert into comments('Donald-2stronk', 'my best friend! #kindomhearts', 'G0fy', '2015-10-13');
