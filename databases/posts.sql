9320a2da31a29961d7e65f9396817dc556550fb7
David Song <songdavid98@gmail.com>
2015-10-13 11:23:34 -0400

    updated posts

--- databases/posts.sql
+++ databases/posts.sql
@@ -1,5 +1,5 @@
-create table posts(title text, body text, username text);
+create table posts(title text, body text, slug text, username text, date date);
 
-insert into posts values('Donald 2stronk', 'donald 2gud, too OP', 'A_Potato');
-insert into posts values('donald duck is retarded', 'can he get any more hilariously retarded?', 'Mickey');
-insert into posts values('We vote for Mr. Trump!', 'We love you, we love you, all the way to the White House!', 'TRUMPSUPERFAN');
+insert into posts values('Donald 2stronk', 'donald 2gud, too OP', 'Donald-2stronk', 'A_Potato', date);
+insert into posts values('donald duck is retarded', 'can he get any more hilariously retarded?', 'donald-duck-is-retarded', 'Mick', date);
+insert into posts values('We vote for Mr. Trump!', 'We love you, we love you, all the way to the White House!', 'We-vote-for-Mr.-Trump', 'TRUMPSUPERFAN', date);
