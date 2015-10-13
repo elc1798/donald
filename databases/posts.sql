a9968c3ff8572534d4f4f7602826da57ceb405e7
David Song <songdavid98@gmail.com>
2015-10-13 11:45:02 -0400

    added dates to posts

--- databases/posts.sql
+++ databases/posts.sql
@@ -1,5 +1,5 @@
 create table posts(title text, body text, slug text, username text, date date);
 
-insert into posts values('Donald 2stronk', 'donald 2gud, too OP', 'Donald-2stronk', 'A_Potato', date);
-insert into posts values('donald duck is retarded', 'can he get any more hilariously retarded?', 'donald-duck-is-retarded', 'Mick', date);
-insert into posts values('We vote for Mr. Trump!', 'We love you, we love you, all the way to the White House!', 'We-vote-for-Mr.-Trump', 'TRUMPSUPERFAN', date);
+insert into posts values('Donald 2stronk', 'donald 2gud, too OP', 'Donald-2stronk', 'A_Potato', '2015-10-11');
+insert into posts values('donald duck is retarded', 'can he get any more hilariously retarded?', 'donald-duck-is-retarded', 'Mick', '2014-12-25');
+insert into posts values('We vote for Mr. Trump!', 'We love you, we love you, all the way to the White House!', 'We-vote-for-Mr.-Trump', 'TRUMPSUPERFAN', '2015-10-10');
