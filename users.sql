5936c7ba8d68f274fabecd8cbd4db58349edef96
tonyli7 <xxtonyxxlixx@gmail.com>
2015-10-16 11:14:54 -0400

    removed users from donald

--- users.sql
+++
@@ -1,9 +0,0 @@
-create table users(firstname text, lastname text, username text, password text);
-
-insert into users values('Barack', 'Obama', 'BO', 'michelle');
-insert into users values('Ben', 'Carson', 'BC', 'wannabeprez');
-insert into users values('Minnie', 'Mouse', 'Min', 'love');
-insert into users values('Mickey', 'Mouse', 'Mick', 'mouse');
-insert into users values('Goofy', 'Go', 'G0fy', '1234');
-insert into users values('Maria', 'Sanchez', 'TRUMPSUPERFAN', 'TRUMP');
-insert into users values('David', 'Song', 'A_Potato', 'DS');
